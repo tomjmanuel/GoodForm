f065db19e2f5956f8b235d0993af7b31f4f2b5c3
Tom Manuel <tom.j.manuel@gmail.com>
2021-01-08 18:19:01 -0600

    can now use react-nat imagepicker

--- screens/Select1/index.js
+++ screens/Select1/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 // this is the Apps Landing page
 
 const styles = StyleSheet.create({
@@ -11,13 +12,42 @@ const styles = StyleSheet.create({
 });
 
 const Select1 = ({ navigation }) => {
+
+  function selectImage() {
+    let options = {
+      title: 'Choose one image',
+       mediaType: 'video',
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+    launchImageLibrary(options, response => {
+          console.log({ response });
+            if (response.didCancel) {
+              console.log('User cancelled photo picker');
+              Alert.alert('You did not select any image');
+            } else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton);
+            } else {
+              let source = { uri: response.uri };
+              console.log({ source });
+            }
+          });
+    }
+
   return (
     <View style={styles.container}>
       <Text>Public Landing Screen</Text>
-        <Button
-          title="Compare Videos now"
-          onPress={() => navigation.navigate('YoutubeCompare')}
-        />
+            <Button
+            title="Select local video"
+            onPress={selectImage}
+            />
+            <Button
+              title="Compare Videos now"
+              onPress={() => navigation.navigate('YoutubeCompare')}
+            />
     </View>
   );
 };
