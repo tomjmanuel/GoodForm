6fe986b738e43feec22e9c2e3a687db99fd46200
Tom Manuel <tom.j.manuel@gmail.com>
2021-01-18 22:26:57 -0600

    zoomable video, column layout

--- screens/Youtube_compare/index.js
+++ screens/Youtube_compare/index.js
@@ -3,18 +3,48 @@ import { View, Text, StyleSheet, Button, Dimensions } from 'react-native';
 import Video from 'react-native-video';
 import YouTube from 'react-native-youtube';
 import 'react-native-gesture-handler';
+import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
 /*
 this is a page where the user compares a local video to a youtube video
 This page comes after select1 page where the user choose a local video and a youtube video
 
 */
+// get page dimensions
+var { height, width } = Dimensions.get('window');
+console.log(width)
+var box_count = 2;
+var controlheight= 200;
+var box_height = height / box_count - controlheight/2;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column'
   },
+  box: {
+    height: box_height,
+    overflow: 'hidden'
+  },
+  box1: {
+    backgroundColor: '#2196F3'
+  },
+  box2: {
+    backgroundColor: '#8BC34A'
+  },
+  box3: {
+    backgroundColor: '#e3aa1a',
+    height: box_height
+  },
+  buttonRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    backgroundColor: 'green',
+    width: '30%',
+    height: '40'
+  }
 });
 
 class VideoComp extends Component {
@@ -61,42 +91,77 @@ class VideoComp extends Component {
         const { width } = Dimensions.get('window');
         const height = width * 0.5625;
         return (
-        <>
-            <YouTube
-              apiKey = "AIzaSyBU2qiNfENE59bF895o5Twoo4qu8MYzW90"
-              videoId="F0PW2sVi2EQ" // The YouTube video ID
-              showFullscreenButton = { false }
-              modestbranding = { true }
-              style={{ alignSelf: 'stretch', height: 300 }}
-              controls = {1}
-              loop = {true}
-              play = {!this.state.paused}
-            />
-            <Video
-              source={{uri: this.props.source}}
-              ref = {ref => this.player = ref}
-              style = {{width: '100%', height}}
-              paused = {this.state.paused}
-              onProgress = {this.handleProgress}
-              onSeek = {this.handleProgress}
-              onLoad = {this.handleLoad}
-              controls = {true}
-              resizeMode = 'contain'
-              progressUpdateInterval = {200}
-            />
-            <Button
-                title="<<"
-                onPress={() => this.stepForward()}
-            />
-            <Button
-                title=">>"
-                onPress={() => this.stepBackward()}
-            />
-            <Button
-              title="Play both"
-              onPress={() => this.togglePlayback()}
-            />
-        </>
+        <View style={styles.container}>
+            <View style={[styles.box, styles.box1]}>
+                <ReactNativeZoomableView
+                   maxZoom={1.5}
+                   minZoom={1}
+                   zoomStep={0.3}
+                   initialZoom={1}
+                   bindToBorders={true}
+                   style={{
+                      padding: 0,
+                      backgroundColor: 'black',
+                   }}
+                >
+                    <YouTube
+                      apiKey = "AIzaSyBU2qiNfENE59bF895o5Twoo4qu8MYzW90"
+                      videoId="F0PW2sVi2EQ" // The YouTube video ID
+                      showFullscreenButton = { false }
+                      modestbranding = { true }
+                      style={{ alignSelf: 'stretch', height: box_height }}
+                      controls = {1}
+                      loop = {true}
+                      play = {!this.state.paused}
+                    />
+                </ReactNativeZoomableView>
+            </View>
+            <View style={[styles.box, styles.box2]}>
+                <ReactNativeZoomableView
+                   maxZoom={1.5}
+                   minZoom={1}
+                   zoomStep={0.3}
+                   initialZoom={1}
+                   bindToBorders={true}
+                   style={{
+                      padding: 0,
+                      backgroundColor: 'black',
+                   }}
+                >
+                    <Video
+                      source={{uri: this.props.source}}
+                      ref = {ref => this.player = ref}
+                      style = {{width: '100%', height}}
+                      paused = {this.state.paused}
+                      onProgress = {this.handleProgress}
+                      onSeek = {this.handleProgress}
+                      onLoad = {this.handleLoad}
+                      controls = {true}
+                      resizeMode = 'contain'
+                      progressUpdateInterval = {200}
+                    />
+                </ReactNativeZoomableView>
+            </View>
+            <View style={styles.box3}>
+                <View style={styles.buttonRow}>
+                    <Button
+                        style={styles.button}
+                        title="<<"
+                        onPress={() => this.stepForward()}
+                    />
+                    <Button
+                        style={styles.button}
+                        title=">>"
+                        onPress={() => this.stepBackward()}
+                    />
+                    <Button
+                        style={styles.button}
+                        title="Play both"
+                        onPress={() => this.togglePlayback()}
+                    />
+                </View>
+            </View>
+        </View>
         )
     }
 }
